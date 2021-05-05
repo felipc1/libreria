46d7a8f187b6b6d6ad8b8f3dba5f5d43d939fd72
David Martinez <dajmarti@bancolombia.com.co>
2021-05-04 22:57:50 -0500

    se agrega y configura libreria de secretmanager del banco para obtener los datos de conexion con la bd de ecopagos

---
+++ src/main/java/co/com/bancolombia/config/JpaConfig.java
@@ -0,0 +1,67 @@
+package co.com.bancolombia.config;
+
+import co.com.bancolombia.secretsmanager.api.GenericManager;
+import co.com.bancolombia.secretsmanager.connector.AWSSecretManagerConnector;
+import co.com.bancolombia.secretsmanager.model.AWSSecretDBModel;
+import co.com.bancolombia.utils.secrets.DBSecret;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public DBSecret dbSecret(@Value("${aws.region}") String region, @Value("${aws.secretManager.secret-name}") String secretName) {
+        GenericManager connector = new AWSSecretManagerConnector(region);
+        try {
+            AWSSecretDBModel secret = connector.getSecret(secretName, AWSSecretDBModel.class);
+            final String url = "jdbc:postgresql://"
+                    .concat(secret.getHost())
+                    .concat(":")
+                    .concat(secret.getPort())
+                    .concat("/")
+                    .concat(secret.getDbname());
+            return DBSecret.builder()
+                    .url(url)
+                    .username(secret.getUsername())
+                    .password(secret.getPassword())
+                    .build();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Bean
+    public DataSource datasource(DBSecret secret, @Value("${spring.datasource.driverClassName}") String driverClass) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(secret.getUrl());
+        config.setUsername(secret.getUsername());
+        config.setPassword(secret.getPassword());
+        config.setDriverClassName(driverClass);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, @Value("${spring.jpa.databasePlatform}") String dialect) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("co.com.bancolombia.domain");
+
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", dialect);
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        em.setJpaProperties(properties);
+
+        return em;
+    }
+}
---
+++ src/main/java/co/com/bancolombia/repository/MontosRepository.java
@@ -0,0 +1,7 @@
+package co.com.bancolombia.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MontosRepository {
+}
