5bc6586600846350fbd6ea2432e079f8a03f4f43
Andrés Felipe Carreño <carrenoaf14@gmail.com>
2021-05-07 14:48:20 -0500

    Andrés - TblPsfMontosRepository

---
+++ src/main/java/co/com/bancolombia/repository/TblPsfMontosRepository.java
@@ -0,0 +1,8 @@
+package co.com.bancolombia.repository;
+
+import co.com.bancolombia.domain.TblPsfMontosModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TblPsfMontosRepository extends CrudRepository<TblPsfMontosModel, Integer> {
+
+}
